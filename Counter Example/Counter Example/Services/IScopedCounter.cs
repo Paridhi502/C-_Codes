3d60ae70fc8d9a837fd6a4bfc35d837fef126602
Paridhi Kumar <114016479+Paridhi502@users.noreply.github.com>
2025-02-09 23:18:55 +0530

    Add files via upload

---
+++ Counter Example/Counter Example/Services/IScopedCounter.cs
@@ -0,0 +1,14 @@
+﻿namespace ServiceLifetimeDemo
+{
+    public interface IScopedCounter
+    {
+        int Count { get; }
+        void Increment();
+    }
+    public class ScopedCounter : IScopedCounter
+    {
+        private int _count;
+        public int Count => _count;
+        public void Increment() => _count++;
+    }
+}
